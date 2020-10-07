16fa78e7dc938e6828f8786e87c79da6922e8ac8
YeahHuang <yeah.huangx@foxmail.com>
2020-10-06 23:20:32 -0400

    initial commit for defining CommentApp & CommentInput

---
+++ src/CommentApp.js
@@ -0,0 +1,17 @@
+//Reference: http://huziketang.mangojuice.top/books/react/lesson14
+import React, { Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
+
+class CommentApp extends Component {
+    render() {
+        return (
+            <div>
+                <CommentInput />
+                <CommentList />
+            </div>
+            )
+    }
+}
+
+export default CommentApp
