9aa9c303369733420c88165d94758dbe610f9c3d
YeahHuang <yeah.huangx@foxmail.com>
2020-10-15 09:47:59 -0400

    add amplify auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import CommentApp from './CommentApp';
 import * as serviceWorker from './serviceWorker';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
     <CommentApp />,
@@ -13,3 +17,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default withAuthenticator(App, true);
